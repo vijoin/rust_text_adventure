108f4f65e869fd7f1759d6e64b81e2aadeccde9e
Victor Inojosa <vijoin@gmail.com>
2022-08-27 14:31:36 -0300

    Initial Commit: Read CSV from filesystem and access to columns

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str = "history.csv";
+
+fn main() {
+    let content = fs::read_to_string(FILENAME).unwrap();
+    // println!("{}", content)
+
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records() {
+        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+    }
+
+}
