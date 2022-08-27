3751d4635e9c22657054d50be1c00cc03e20ae37
Victor Inojosa <vijoin@gmail.com>
2022-08-27 16:31:46 -0300

    Get health value and Create list with all story rows

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,35 @@ const FILENAME: &str = "history.csv";
 fn main() {
 
     #[derive(Debug)]
-    struct RowStory {
+    struct StoryRow {
         row_type: String,
         tag: String,
         text: String,
         health: i32
     }
 
+    let mut story_data: Vec<StoryRow> = vec![]; 
+
     let content = fs::read_to_string(FILENAME).unwrap();
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
     for result in rdr.records() {
         let result = result.unwrap().clone();
+        let health: i32 = get_column_text(&result, 3).parse().unwrap_or(0);
 
-        let row_story = RowStory {
+        let row_story = StoryRow {
             row_type: get_column_text(&result, 0),
             tag: get_column_text(&result, 1),
             text: get_column_text(&result, 2),
-            health: 0
+            health: health
         };
-        println!("RowStory: {:?}", row_story);
+
+        story_data.push(row_story);
+        // println!("StoryRow: {:?}", row_story);
     }
+
+    println!("Whole history: {:?}", story_data);
+    
 
 }
 
