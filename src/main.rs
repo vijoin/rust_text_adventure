4f80b4f7be615e0b6b40ea813fd4299cc64bf56f
Victor Inojosa <vijoin@gmail.com>
2022-08-27 16:15:37 -0300

    create data structure to represen a Story Row, create instances for every row and print it

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,33 @@ use std::{fs};
 const FILENAME: &str = "history.csv";
 
 fn main() {
-    let content = fs::read_to_string(FILENAME).unwrap();
-    // println!("{}", content)
 
+    #[derive(Debug)]
+    struct RowStory {
+        row_type: String,
+        tag: String,
+        text: String,
+        health: i32
+    }
+
+    let content = fs::read_to_string(FILENAME).unwrap();
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
     for result in rdr.records() {
-        let result = result.unwrap();
-        println!("Texto: {}", get_column_text(result, 2));
+        let result = result.unwrap().clone();
+
+        let row_story = RowStory {
+            row_type: get_column_text(&result, 0),
+            tag: get_column_text(&result, 1),
+            text: get_column_text(&result, 2),
+            health: 0
+        };
+        println!("RowStory: {:?}", row_story);
     }
 
 }
 
 
-fn get_column_text(result: StringRecord, column_number: usize) -> String {
+fn get_column_text(result: &StringRecord, column_number: usize) -> String {
     return result.get(column_number).unwrap().trim().to_string();
 }
