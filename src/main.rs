d3b4110ec7bf408011dec834a10afffa897765b5
Victor Inojosa <vijoin@gmail.com>
2022-08-27 17:06:27 -0300

    Simply StoryRow creation with Impl new

--- src/main.rs
+++ src/main.rs
@@ -13,32 +13,35 @@ fn main() {
         health: i32
     }
 
-    let mut story_data: Vec<StoryRow> = vec![]; 
+    impl StoryRow{
 
+        fn new(row: StringRecord) -> StoryRow{
+            let health: i32 = StoryRow::get_column_text(&row, 3).parse().unwrap_or(0);
+            return StoryRow {
+                row_type: StoryRow::get_column_text(&row, 0),
+                tag: StoryRow::get_column_text(&row, 1),
+                text: StoryRow::get_column_text(&row, 2),
+                health: health
+            }
+        }
+
+        fn get_column_text(row: &StringRecord, column_number: usize) -> String {
+            return row.get(column_number).unwrap().trim().to_string();
+        }
+    }
+        
+        
+    let mut story_data: Vec<StoryRow> = vec![]; 
     let content = fs::read_to_string(FILENAME).unwrap();
     let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
     for result in rdr.records() {
-        let result = result.unwrap().clone();
-        let health: i32 = get_column_text(&result, 3).parse().unwrap_or(0);
-
-        let row_story = StoryRow {
-            row_type: get_column_text(&result, 0),
-            tag: get_column_text(&result, 1),
-            text: get_column_text(&result, 2),
-            health: health
-        };
-
+        let result = result.unwrap();
+        let row_story = StoryRow::new(result);
         story_data.push(row_story);
         // println!("StoryRow: {:?}", row_story);
     }
 
     println!("Whole history: {:?}", story_data);
     
-
-}
-
-
-fn get_column_text(result: &StringRecord, column_number: usize) -> String {
-    return result.get(column_number).unwrap().trim().to_string();
 }
